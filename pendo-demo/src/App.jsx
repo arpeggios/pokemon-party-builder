0c1536c7974c9ffbedda53476cf70a19f78dab4a
JM <jvcmoore@gmail.com>
2024-09-12 00:26:38 -0400

    display random pokemon on page load; make data global by moving fetch up to app component

--- pendo-demo/src/App.jsx
+++ pendo-demo/src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import { PokemonSearch } from './components/PokemonSearch'
 
 function App() {
+  // todo: put this data fetching in a separate file?
+  const [pokemon, setPokemon] = useState();
+
+  const displayPokemon = async (theInput) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${theInput}`)
+    const data = await response.json()
+
+    setPokemon(data)
+  }
+
+  function randomPokemon(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  useEffect(() => {
+    const randomNum = randomPokemon(1, 1025);
+    displayPokemon(randomNum);
+  }, [])
+
   return (
     <>
-      <PokemonSearch />
+      <PokemonSearch pkmnData={pokemon} displayPokemon={displayPokemon}/>
     </>
   )
 }
--- pendo-demo/src/components/PokemonSearch.jsx
+++ pendo-demo/src/components/PokemonSearch.jsx
@@ -1,22 +1,12 @@
-// todo: move api call to a parent component and make pikachu (or random) the default
-// todo: make a chart for stats
+// todo: make a button to show a random pokemon
 // todo: use context api to hold "captured" pokemon. Capture up to six. Click a captured one to display it. Have a button to release it.
 // todo: use reducers instead of useState
 // todo: use redux instead of context api
+// todo: make a chart for stats
 
-import { useState } from 'react'
 import { PokemonDisplay } from "./PokemonDisplay"
 
-export function PokemonSearch() {
-  const [pokemon, setPokemon] = useState();
-
-  const displayPokemon = async (theInput) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${theInput}`)
-    const data = await response.json()
-
-    setPokemon(data)
-  }
-
+export function PokemonSearch({ pkmnData, displayPokemon }) {
   function handleSubmit(e) {
     e.preventDefault();
     displayPokemon(e.target.querySelector("#entered-pokemon").value.toLowerCase())
@@ -32,8 +22,8 @@ export function PokemonSearch() {
           style={{ width: "200px" }}
         />
       </form>
-      {{ pokemon } &&
-        <PokemonDisplay pkmn={pokemon} />
+      {{ pkmnData } &&
+        <PokemonDisplay pkmn={pkmnData} />
       }
     </>
   )
