6d07b13f31c727024533a4a584655d333f45d438
JM <jvcmoore@gmail.com>
2024-09-11 22:10:48 -0400

    cleanup

--- pendo-demo/src/components/PokemonDisplay.jsx
+++ pendo-demo/src/components/PokemonDisplay.jsx
@@ -1,12 +1,12 @@
-export function PokemonDisplay({pkmn}) {
+export function PokemonDisplay({ pkmn }) {
   if (pkmn) {
     console.log(pkmn)
     return (
       <>
-          <div>Name: {pkmn.name}</div>
-          <div>ID: {pkmn.id}</div>
-          <img src={pkmn.sprites.front_default} alt="" />
-          <div>Type: {pkmn.types[0].type.name} </div>
+        <div>Name: {pkmn.name}</div>
+        <div>ID: {pkmn.id}</div>
+        <img src={pkmn.sprites.front_default} alt="" />
+        <div>Type: {pkmn.types[0].type.name} </div>
       </>
     )
   }
--- pendo-demo/src/components/PokemonSearch.jsx
+++ pendo-demo/src/components/PokemonSearch.jsx
@@ -1,48 +1,36 @@
-import { useState, useEffect } from 'react'
+// todo: move api call to a parent component and make pikachu (or random) the default
+// todo: make a chart for stats
+// todo: use context api to hold "captured" pokemon. Capture up to six. Click a captured one to display it. Have a button to release it.
+// todo: use reducers instead of useState
+// todo: use redux instead of context api
+
+import { useState } from 'react'
 import { PokemonDisplay } from "./PokemonDisplay"
 
 export function PokemonSearch() {
-  // todo: move api call to a parent component and make pikachu (or random) the default
-  // todo: make a chart for stats
-  // todo: use context api to hold "captured" pokemon. Capture up to six. Click a captured one to display it. Have a button to release it.
-  // todo: use reducers instead of useState
-  // todo: use redux instead of context api
   const [pokemon, setPokemon] = useState();
 
   const displayPokemon = async (theInput) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${theInput}`)
-
     const data = await response.json()
 
-    console.log("data")
-    console.log(data)
-
     setPokemon(data)
-
-    console.log(`pokemon: ${pokemon}`)
   }
-
-  // useEffect(() => {
-  //   displayPokemon();
-  // }, [])
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
-
-    console.log(e.target.querySelector("#entered-pokemon").value);
-
-    // setPokemon(e.target.querySelector("#entered-pokemon").value)
-
     displayPokemon(e.target.querySelector("#entered-pokemon").value.toLowerCase())
-
-    console.log(pokemon)
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input id="entered-pokemon" type="text" placeholder="Enter Pokemon name or ID" style={{ width: "200px" }} />
+        <input
+          id="entered-pokemon"
+          type="text"
+          placeholder="Enter Pokemon name or ID"
+          style={{ width: "200px" }}
+        />
       </form>
       {{ pokemon } &&
         <PokemonDisplay pkmn={pokemon} />
