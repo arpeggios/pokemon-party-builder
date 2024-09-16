d8778314d24bbcee4eed34891173c0de09ab284b
JM <jvcmoore@gmail.com>
2024-09-16 03:38:22 -0400

    minor title and about page edits

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export function About() {
         About
       </h1>
       <p>
-        I'm Jason, and this is an app I'm building in my spare time that was started on September 10, 2024. It is made with React and the <a href="https://pokeapi.co/" target="_blank">Pokeapi</a>. It saves your party automatically to local storage.
+        Hi, I'm Jason, and this is an app I'm building in my spare time that was started on September 10, 2024. It's made with React and the <a href="https://pokeapi.co/" target="_blank">Pokeapi</a>, and it saves your party automatically to local storage.
       </p>
     </>
   )
