6af36619808fc2f68ae91835594b04711729dfb5
JM <jvcmoore@gmail.com>
2024-09-25 02:34:36 -0400

    updated about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ export function About() {
         About
       </Typography>
       <Typography>
-        Hi, I'm Jason, and this is an app I'm building in my spare time that was started on September 10, 2024. It's made with React and the <Link href="https://pokeapi.co/" target="_blank" rel="noopener">Pokeapi</Link>, and it saves your party automatically to local storage.
+        Hi, I'm Jason, and this is an app I'm building in my spare time that was started on September 10, 2024. It's made with React, Material UI, and the <Link href="https://pokeapi.co/" target="_blank" rel="noopener">Pokeapi</Link>, and it uses local storage to save your party automatically.
       </Typography>
     </>
   )
